4fc158e03dda6de4ff0d3052a190056263ce7efd
Jean-Rene Branaa <jrbranaa@macbaby.localdomain>
2026-02-17 16:32:50 -0800

    configured neogit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- Required
+    'sindrets/diffview.nvim', -- Optional - for enhanced diffs
+  },
+  cmd = 'Neogit', -- Lazy load on command
+  config = true, -- Automatically runs setup()
+}
